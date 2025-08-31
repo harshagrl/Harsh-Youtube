fa65586d9d6c75ed456909d68270a344543792dd
harsh <harshkr.agrl@gmail.com>
2025-08-31 15:13:17 +0530

    api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,17 +7,9 @@ export const YOUTUBE_LOGO =
 export const USER_ICON =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR81iX4Mo49Z3oCPSx-GtgiMAkdDop2uVmVvw&s";
 
-<<<<<<< HEAD
 export const YOUTUBE_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   import.meta.env.VITE_API_KEY;
-=======
-const API_KEY = "AIzaSyCnJU3N9AU_aHLA9LLIfSv3Eskp_gVZCRk";
-
-export const YOUTUBE_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
-  API_KEY;
->>>>>>> 995a5db1ca5c1645108c2267b9908fa39d742675
 
 export const YOUTUBE_SEARCH_API =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
